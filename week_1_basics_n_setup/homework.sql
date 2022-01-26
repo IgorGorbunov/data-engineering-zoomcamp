9b53ffe57362a5293c01875ff465d265fa8f0cb9
Igor Gorbunov <give@list.ru>
2022-01-27 00:41:02 +0300

    Update homework.sql

--- week_1_basics_n_setup/homework.sql
+++ week_1_basics_n_setup/homework.sql
@@ -1,21 +1,21 @@
 
---3
+--3 53024
 select
 count(*)
-FROM public.yellow_tripdata
+FROM public.yellow_taxi_trips
 where to_char(tpep_pickup_datetime::date, 'MM-dd') = '01-15';
 
---4
+--4 2021-01-20
 with t as (
 SELECT 
 tpep_pickup_datetime,
 max(tip_amount) as tipmax
-FROM public.yellow_tripdata
+FROM public.yellow_taxi_trips
 group by tpep_pickup_datetime
 )
 
-select tpep_pickup_datetime
-from public.yellow_tripdata
+select tpep_pickup_datetime::date
+from public.yellow_taxi_trips
 where tip_amount = (
 select max(tipmax)
 from t 
@@ -23,16 +23,26 @@ where to_char(tpep_pickup_datetime::date, 'MM') = '01'
 );
 
 
---5
-select dolocationid, count(*)
-from public.yellow_tripdata
-where to_char(tpep_pickup_datetime::date, 'MM-dd') = '01-14'
-and pulocation = 'Central Park'
-group by dolocationid
-order by 2;
+--5 Upper East Side South
+select zdo."Zone", count(*)
+from public.yellow_taxi_trips t
+inner join public.zones zpu on
+    t."PULocationID" = zpu."LocationID"
+inner join public.zones zdo on
+    t."DOLocationID" = zdo."LocationID"
+where to_char(t.tpep_pickup_datetime::date, 'MM-dd') = '01-14'
+and zpu."Zone" = 'Central Park'
+group by zdo."Zone"
+order by 2 DESC
+limit 1;
 
---6
-select avg(total_amount), pulocation , dolocation
-from public.yellow_tripdata
-group by pulocationid , dolocationid 
-order by 1 desc;
+--6 Alphabet City / Unknown
+select avg(total_amount), zpu."Zone" , zdo."Zone"
+from public.yellow_taxi_trips t
+inner join public.zones zpu on
+    t."PULocationID" = zpu."LocationID"
+inner join public.zones zdo on
+    t."DOLocationID" = zdo."LocationID"
+group by zpu."Zone" , zdo."Zone"
+order by 1 desc
+limit 1;
